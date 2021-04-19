beac10b2a7b3de50bb928c4f8501c571dfcdefaa
Shauray <shauray9@gmail.com>
2021-04-19 22:54:49 +0530

    Neural Style Transfer

---
+++ stylized_NP/neural_transfer.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torchvision.transforms as transforms
+import torchvision.models import models
+
+from utils import *
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
