e9e467e7d84d44461da110f4f553a59a425571c1
Shauray <shauray9@gmail.com>
2021-04-19 22:59:07 +0530

    added some utility functions

--- stylized_NP/utils.py
+++ stylized_NP/utils.py
@@ -1,5 +1,20 @@
-import torch
+import torch.nn as nn
 import numpy as np
 from PIL import Image 
 import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+
+
+def loader(imsize):
+    loader = transforms.compose([
+        transforms.Resize=(imsize),
+        transforms.ToTensor()])
+    return loader
+
+def img_loader(image, device):
+    image = Image.open(image_name)
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+
 
