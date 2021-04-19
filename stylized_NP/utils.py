e6902402b53cd5f4b4fd905f327a16315151c2bd
Shauray <shauray9@gmail.com>
2021-04-19 23:50:42 +0530

    losses

--- stylized_NP/utils.py
+++ stylized_NP/utils.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 import torch
 import numpy as np
 from PIL import Image 
@@ -28,5 +29,38 @@ def imshow(tensor, title=None):
         plt.title(title)
     plt.imshow(image)
     plt.pause(1)
+
+class ContentLoss(nn.Module):
+    def __init__(self, target):
+        super(ContenLoss, self).__init__()
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+
+def gram_matrix(input):
+    a,b,c,d = input.size()
+
+    features = input.view(a*b, c*d)
+    G = torch.mm(features, features.t())
+
+    return G.div(a*b*c*d)
+
+
+class styleLoss(nn.Module):
+    def __init__(self, target_featur):
+        super(styleLoss, self).__init__()
+
+        self.target = gram_matrix(target_features).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+
+
 
 
